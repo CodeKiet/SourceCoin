d7c523054ce1d33cdedd9c40fda9f12ffd82e543
CoinsSource <support@coinssource.com>
2014-05-06 09:22:55 -0500

    Hard Fork at block 20,000
    Update for POS implementation at block 20,000

--- src/main.h
+++ src/main.h
@@ -24,7 +24,7 @@ class CAddress;
 class CInv;
 class CRequestTracker;
 class CNode;
-
+static const int LAST_POW_BLOCK = 20000;
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
